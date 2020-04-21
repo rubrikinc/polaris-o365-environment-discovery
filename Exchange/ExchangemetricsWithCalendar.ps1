c5ebb9c373f15d438ce178e0d819ba219e89facb
Manoj Verma <Manoj75@gmail.com>
2020-04-20 19:16:35 -0700

    Added new data elements in the calendar metrics

--- Exchange/ExchangemetricsWithCalendar.ps1
+++ Exchange/ExchangemetricsWithCalendar.ps1
@@ -19,13 +19,15 @@
 ################################################################################################################################################################
 #Accept input parameters
 Param(
-[Parameter(Position=0, Mandatory=$false, ValueFromPipeline=$true)]
+[Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)]
 [string] $Office365Username,
-[Parameter(Position=1, Mandatory=$false, ValueFromPipeline=$true)]
-[string] $Office365Password,
+[Parameter(Position=1, Mandatory=$true, ValueFromPipeline=$true)]
+[Security.SecureString] $Office365Password,
 [Parameter(Position=2, Mandatory=$false, ValueFromPipeline=$true)]
 [string] $UserIDFile
 )
+
+
 #Constant Variables
 #$OutputFile = "MailboxSizes.csv" #The CSV Output file that is created, change for your purposes
 #Main
@@ -34,48 +36,276 @@ Function Main
     #Remove all existing Powershell sessions
     Get-PSSession | Remove-PSSession
     #Call ConnectTo-ExchangeOnline function with correct credentials
-    $Office365Username ="admin.o365@rdnn14.onmicrosoft.com" 
-    $Office365Password="scaledata12#$"
     ConnectTo-ExchangeOnline -Office365AdminUsername $Office365Username -Office365AdminPassword $Office365Password
     #Prepare Output file with headers
-    #Out-File -FilePath $OutputFile -InputObject "UserPrincipalName,NumberOfItems,MailboxSize" -Encoding UTF8
+
+    $MailBoxDataPath = "MailboxReport.csv"
+    $CalendarDataPath = "CalendarReport.csv"
+
     #Check if we have been passed an input file path
     if ($userIDFile -ne "")
     {
-    #We have an input file, read it into memory
-    $objUsers = import-csv -Header "UserPrincipalName" $UserIDFile
+        #We have an input file, read it into memory
+        $objUsers = import-csv -Header "UserPrincipalName" $UserIDFile
+        #write-host "*******************"
+        #write-host $objUsers
+        #write-host "*******************"
+
+        $Results =@()
+        $CalendarResults =@()
+        $MailboxResults =@()
+
+        $obj=@()
+        ForEach ($User in $objUsers)
+        {
+            
+            $UserCalender=Get-MailboxFolderStatistics -Identity $User.UserPrincipalName  –FolderScope 'Calendar'  | 
+            Select-object   @{
+                            Label = "Identity"
+                            Expression = { if ($_.Identity) { $_.Identity } else { "No Data" } }
+                            },
+                            @{
+                            Label = "Date"
+                            Expression = { if ($_.Date) { $_.Date } else { "No Data" } }
+                            },
+                            @{
+                            Label = "CreationTime"
+                            Expression = { if ($_.CreationTime) { $_.CreationTime } else { "No Data" } }
+                            },
+                            @{
+                            Label = "LastModifiedTime"
+                            Expression = { if ($_.LastModifiedTime) { $_.LastModifiedTime } else { "No Data" } }
+                            },
+                            @{
+                            Label = "Name"
+                            Expression = { if ($_.Name) { $_.Name } else { "No Data" } }
+                            },
+                            @{
+                            Label = "FolderPath"
+                            Expression = { if ($_.FolderPath) { $_.FolderPath } else { "No Data" } }
+                            },
+                            @{
+                            Label = "FolderId"
+                            Expression = { if ($_.FolderId) { $_.FolderId } else { "No Data" } }
+                            },
+                            @{
+                            Label = "Movable"
+                            Expression = { if ($_.Movable) { $_.Movable } else { "No Data" } }
+                            },
+                            @{
+                            Label = "VisibleItemsInFolder"
+                            Expression = { if ($_.VisibleItemsInFolder) { $_.VisibleItemsInFolder } else { "No Data" } }
+                            },
+                            @{
+                            Label = "HiddenItemsInFolder"
+                            Expression = { if ($_.HiddenItemsInFolder) { $_.HiddenItemsInFolder } else { "No Data" } }
+                            },
+                            @{
+                            Label = "ItemsInFolder"
+                            Expression = { if ($_.ItemsInFolder) { $_.ItemsInFolder } else { "No Data" } }
+                            },
+                            @{
+                            Label = "DeletedItemsInFolder"
+                            Expression = { if ($_.ItemsInFolder) { $_.ItemsInFolder } else { "No Data" } }
+                            },
+                            @{
+                            Label = "FolderSize"
+                            Expression = { if ($_.ItemsInFolder) { $_.ItemsInFolder } else { "No Data" } }
+                            },
+                            @{
+                            Label = "FolderAndSubfolderSize"
+                            Expression = { if ($_.FolderAndSubfolderSize) { $_.FolderAndSubfolderSize } else { "No Data" } }
+                            },
+                            @{
+                            Label = "OldestItemReceivedDate"
+                            Expression = { if ($_.OldestItemReceivedDate) { $_.OldestItemReceivedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "NewestItemReceivedDate"
+                            Expression = { if ($_.NewestItemReceivedDate) { $_.NewestItemReceivedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "OldestDeletedItemReceivedDate"
+                            Expression = { if ($_.OldestDeletedItemReceivedDate) { $_.OldestDeletedItemReceivedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "NewestDeletedItemReceivedDate"
+                            Expression = { if ($_.NewestDeletedItemReceivedDate) { $_.NewestDeletedItemReceivedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "OldestItemLastModifiedDate"
+                            Expression = { if ($_.OldestItemLastModifiedDate) { $_.OldestItemLastModifiedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "NewestItemLastModifiedDate"
+                            Expression = { if ($_.NewestItemLastModifiedDate) { $_.NewestItemLastModifiedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "OldestDeletedItemLastModifiedDate"
+                            Expression = { if ($_.OldestDeletedItemLastModifiedDate) { $_.OldestDeletedItemLastModifiedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "NewestDeletedItemLastModifiedDate"
+                            Expression = { if ($_.NewestDeletedItemLastModifiedDate) { $_.NewestDeletedItemLastModifiedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "TopSubjectSize"
+                            Expression = { if ($_.TopSubjectSize) { $_.TopSubjectSize } else { "No Data" } }
+                            },
+                            @{
+                            Label = "TopSubjectCount"
+                            Expression = { if ($_.TopSubjectCount) { $_.TopSubjectCount } else { "No Data" } }
+                            },
+                            @{
+                            Label = "IsValid"
+                            Expression = { if ($_.IsValid) { $_.IsValid } else { "No Data" } }
+                            }
+
+            #$UserCalender
+            $userMailbox = get-mailboxstatistics -Identity $User.UserPrincipalName | Select DisplayName,DeletedItemCount,ItemCount,TotalDeletedItemSize,TotalItemSize,MessageTableTotalSize,MessageTableAvailableSize,AttachmentTableTotalSize,AttachmentTableAvailableSize,OtherTablesTotalSize,OtherTablesAvailableSize,IsEncrypted,LastInteractionTime,LastLoggedOnUserAccount,LastLogoffTime,LastLogonTime,AssociatedItemCount ,IsValid,IsArchiveMailBox,MailBoxType 
+            #$userMailbox
+            # Collect Calendar information
+            if ($UserCalender.length -eq 1)
+            {
+                $CalendarResults += [pscustomobject]$UserCalender
+            }
+            else
+            {
+                ForEach ($obj in $UserCalender)
+                {
+                    $CalendarResults += [pscustomobject]$obj
+                }
+            }
+            #write-host $CalendarResults
+            # Collect Mailbox information
+            if ($userMailbox.length -eq 1)
+            {
+                $MailboxResults += [pscustomobject]$userMailbox
+            }
+            else
+            {
+                ForEach ($mailBoxObj in $userMailbox)
+                {
+                    $MailboxResults += [pscustomobject]$mailBoxObj
+                }
+            }
+            #write-host $MailboxResults
+        }
+        $CalendarResults | Export-CSV  $CalendarDataPath
+        $MailboxResults | Export-CSV  $MailBoxDataPath
     }
     else
     {
     #No input file found, gather all mailboxes from Office 365
-        $objUsers = get-mailbox  -ResultSize Unlimited | get-mailboxstatistics | Select DisplayName,DeletedItemCount,ItemCount,TotalDeletedItemSize,TotalItemSize,MessageTableTotalSize,MessageTableAvailableSize,AttachmentTableTotalSize,AttachmentTableAvailableSize,OtherTablesTotalSize,OtherTablesAvailableSize,IsEncrypted,LastInteractionTime,LastLoggedOnUserAccount,LastLogoffTime,LastLogonTime,AssociatedItemCount ,IsValid,IsArchiveMailBox,MailBoxType | Export-CSV  MailboxReport.csv
-        $objUsersCal = get-mailbox  -ResultSize Unlimited | Get-MailboxFolderStatistics  –FolderScope 'Calendar'  | Select Identity,Name , FolderPath,CreationTime,FolderId,HiddenItemsInFolder,ItemsInFolder,DeletedItemsInFolder,FolderSize | Export-CSV  CalFile.csv
+        $objUsers = get-mailbox  -ResultSize Unlimited | get-mailboxstatistics | Select DisplayName,DeletedItemCount,ItemCount,TotalDeletedItemSize,TotalItemSize,MessageTableTotalSize,MessageTableAvailableSize,AttachmentTableTotalSize,AttachmentTableAvailableSize,OtherTablesTotalSize,OtherTablesAvailableSize,IsEncrypted,LastInteractionTime,LastLoggedOnUserAccount,LastLogoffTime,LastLogonTime,AssociatedItemCount ,IsValid,IsArchiveMailBox,MailBoxType | Export-CSV  $MailBoxDataPath
+        $objUsersCal = get-mailbox  -ResultSize Unlimited | Get-MailboxFolderStatistics  –FolderScope 'Calendar' | 
+        Select-object   @{
+                            Label = "Identity"
+                            Expression = { if ($_.Identity) { $_.Identity } else { "No Data" } }
+                            },
+                            @{
+                            Label = "Date"
+                            Expression = { if ($_.Date) { $_.Date } else { "No Data" } }
+                            },
+                            @{
+                            Label = "CreationTime"
+                            Expression = { if ($_.CreationTime) { $_.CreationTime } else { "No Data" } }
+                            },
+                            @{
+                            Label = "LastModifiedTime"
+                            Expression = { if ($_.LastModifiedTime) { $_.LastModifiedTime } else { "No Data" } }
+                            },
+                            @{
+                            Label = "Name"
+                            Expression = { if ($_.Name) { $_.Name } else { "No Data" } }
+                            },
+                            @{
+                            Label = "FolderPath"
+                            Expression = { if ($_.FolderPath) { $_.FolderPath } else { "No Data" } }
+                            },
+                            @{
+                            Label = "FolderId"
+                            Expression = { if ($_.FolderId) { $_.FolderId } else { "No Data" } }
+                            },
+                            @{
+                            Label = "Movable"
+                            Expression = { if ($_.Movable) { $_.Movable } else { "No Data" } }
+                            },
+                            @{
+                            Label = "VisibleItemsInFolder"
+                            Expression = { if ($_.VisibleItemsInFolder) { $_.VisibleItemsInFolder } else { "No Data" } }
+                            },
+                            @{
+                            Label = "HiddenItemsInFolder"
+                            Expression = { if ($_.HiddenItemsInFolder) { $_.HiddenItemsInFolder } else { "No Data" } }
+                            },
+                            @{
+                            Label = "ItemsInFolder"
+                            Expression = { if ($_.ItemsInFolder) { $_.ItemsInFolder } else { "No Data" } }
+                            },
+                            @{
+                            Label = "DeletedItemsInFolder"
+                            Expression = { if ($_.ItemsInFolder) { $_.ItemsInFolder } else { "No Data" } }
+                            },
+                            @{
+                            Label = "FolderSize"
+                            Expression = { if ($_.ItemsInFolder) { $_.ItemsInFolder } else { "No Data" } }
+                            },
+                            @{
+                            Label = "FolderAndSubfolderSize"
+                            Expression = { if ($_.FolderAndSubfolderSize) { $_.FolderAndSubfolderSize } else { "No Data" } }
+                            },
+                            @{
+                            Label = "OldestItemReceivedDate"
+                            Expression = { if ($_.OldestItemReceivedDate) { $_.OldestItemReceivedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "NewestItemReceivedDate"
+                            Expression = { if ($_.NewestItemReceivedDate) { $_.NewestItemReceivedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "OldestDeletedItemReceivedDate"
+                            Expression = { if ($_.OldestDeletedItemReceivedDate) { $_.OldestDeletedItemReceivedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "NewestDeletedItemReceivedDate"
+                            Expression = { if ($_.NewestDeletedItemReceivedDate) { $_.NewestDeletedItemReceivedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "OldestItemLastModifiedDate"
+                            Expression = { if ($_.OldestItemLastModifiedDate) { $_.OldestItemLastModifiedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "NewestItemLastModifiedDate"
+                            Expression = { if ($_.NewestItemLastModifiedDate) { $_.NewestItemLastModifiedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "OldestDeletedItemLastModifiedDate"
+                            Expression = { if ($_.OldestDeletedItemLastModifiedDate) { $_.OldestDeletedItemLastModifiedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "NewestDeletedItemLastModifiedDate"
+                            Expression = { if ($_.NewestDeletedItemLastModifiedDate) { $_.NewestDeletedItemLastModifiedDate } else { "No Data" } }
+                            },
+                            @{
+                            Label = "TopSubjectSize"
+                            Expression = { if ($_.TopSubjectSize) { $_.TopSubjectSize } else { "No Data" } }
+                            },
+                            @{
+                            Label = "TopSubjectCount"
+                            Expression = { if ($_.TopSubjectCount) { $_.TopSubjectCount } else { "No Data" } }
+                            },
+                            @{
+                            Label = "IsValid"
+                            Expression = { if ($_.IsValid) { $_.IsValid } else { "No Data" } }
+                            }| Export-CSV  $CalendarDataPath
     }
-    #$DataPath = "CalendarUsage.csv"
-    #$Results =@()
-    #$CalendarResults =@()
-    #$obj=@()
-    #$objmMilboxUsers=get-mailbox
-    #write-host $objmMilboxUsers
-    #ForEach ($User in $objmMilboxUsers)
-    #{
-    #    $UserCalender=Get-MailboxFolderStatistics $User.Alias –FolderScope 'Calendar'  | Select Identity,Name , FolderPath,CreationTime,FolderId,HiddenItemsInFolder,ItemsInFolder,DeletedItemsInFolder,FolderSize 
-    #    if ($UserCalender.length -eq 1)
-    #    {
-    #        $CalendarResults += [pscustomobject]$UserCalender
-    #    }
-    #    else
-    #    {
-    #        ForEach ($obj in $UserCalender)
-    #        {
-    #            $CalendarResults += [pscustomobject]$obj
-    #        }
-    #    }
-    #}
-    #write-host "Finished"
-    #write-host $CalendarResults
-    #$CalendarResults | Export-CSV  "CalendarFile.csv"
-    #}
+
+    write-host "Finished"
+    write-host "Calendar file created: $CalendarDataPath"
+    write-host "Mailbox file created: $MailBoxDataPath"
+
+
 
     Get-PSSession | Remove-PSSession
 }
@@ -103,12 +333,14 @@ Position=0)]
 [Parameter(
 Mandatory=$true,
 Position=1)]
-[String]$Office365AdminPassword
+[Security.SecureString]$Office365AdminPassword
 )
 
 #Encrypt password for transmission to Office365
-$SecureOffice365Password = ConvertTo-SecureString -AsPlainText $Office365AdminPassword -Force
+#$SecureOffice365Password = ConvertTo-SecureString -AsPlainText $Office365AdminPassword -Force
+$SecureOffice365Password=$Office365AdminPassword
 #Build credentials object
+#write-host $Office365AdminPassword
 $Office365Credentials = New-Object System.Management.Automation.PSCredential $Office365AdminUsername, $SecureOffice365Password
 #Create remote Powershell session
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $Office365credentials -Authentication Basic –AllowRedirection
